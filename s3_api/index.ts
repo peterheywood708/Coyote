5e38ac27c702e131e66daf0413b08fe54b9797d8
Peter Heywood <peterheywood708@hotmail.com>
2025-04-23 22:24:29 +0100

    Boilerplate for API

--- s3_api/index.ts
+++ s3_api/index.ts
@@ -1,1 +1,21 @@
-console.log("Hi from S3");
+import AWS from "aws-sdk";
+import fs, { unlinkSync } from "fs";
+import express, { Request, Response } from "express";
+import * as dotenv from "dotenv";
+import multer from "multer";
+import { CognitoJwtVerifier } from "aws-jwt-verify";
+
+const cors = require("cors");
+const app = express();
+app.use(cors());
+dotenv.config();
+
+const port: string = process.env.PORT || "3002";
+
+app.get("/", async (req: Request, res: Response) => {
+  res.send("Hi!");
+});
+
+app.listen(port, () => {
+  console.log(`S3 API running on port ${port}`);
+});
