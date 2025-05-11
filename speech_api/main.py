e64a3e3287fb3836ba880eeafd81d13812cde935
Peter Heywood <peterheywood708@hotmail.com>
2025-05-11 13:16:51 +0100

    Updated python script

--- speech_api/main.py
+++ speech_api/main.py
@@ -4,9 +4,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+#Make our directories if they don't already exist
+if not os.path.exists('in'):
+    os.makedirs('in')
+if not os.path.exists('out'):
+    os.makedirs('out')
+
+#Default route
 @app.route("/")
 def hello():
-    return "Hi"
+    return "Speech API service is running"
 
 if __name__ == "__main__":
     app.run()
