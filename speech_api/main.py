eee40d6cf2211cb7f24e20e4b04d74f20def0ba4
Peter Heywood <peterheywood708@hotmail.com>
2025-07-29 21:57:30 +0100

    test

--- speech_api/main.py
+++ speech_api/main.py
@@ -1,19 +1,32 @@
 import os
 import json
-from flask import Flask
+from flask import Flask, request, send_file
 
 app = Flask(__name__)
+inFolder = 'in'
+outFolder = 'out'
 
 #Make our directories if they don't already exist
-if not os.path.exists('in'):
-    os.makedirs('in')
-if not os.path.exists('out'):
-    os.makedirs('out')
+if not os.path.exists(inFolder):
+    os.makedirs(inFolder)
+if not os.path.exists(outFolder):
+    os.makedirs(outFolder)
 
 #Default route
 @app.route("/")
 def hello():
     return "Speech API service is running"
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    try:
+        file = request.files['file']
+        file.save(os.path.join('in',file.filename))
+        return f'{file.filename} uploaded successfully'
+    except KeyError:
+        return 'No file uploaded',400
+    except Exception as e:
+        return f'An error occured: {str(e)}',500
 
 if __name__ == "__main__":
     app.run()
