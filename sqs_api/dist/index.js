16d17b0620b63f3438d6f389435ce2013b88be80
Peter Heywood <peterheywood708@hotmail.com>
2025-08-18 23:29:40 +0100

    Added SQS API

--- sqs_api/dist/index.js
+++ sqs_api/dist/index.js
@@ -32,17 +32,32 @@ var __importStar = (this && this.__importStar) || (function () {
         return result;
     };
 })();
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
 const dotenv = __importStar(require("dotenv"));
 const aws_jwt_verify_1 = require("aws-jwt-verify");
+const aws_sdk_1 = __importDefault(require("aws-sdk"));
 const cors = require("cors");
 const app = (0, express_1.default)();
 app.use(cors());
+app.use(body_parser_1.default.urlencoded({ extended: false }));
+app.use(body_parser_1.default.json());
 dotenv.config();
+// Configuration of AWS
+const sqs = new aws_sdk_1.default.SQS({ apiVersion: process.env.API_VERSION });
 // Verifier that expects valid access tokens:
 const verifier = aws_jwt_verify_1.CognitoJwtVerifier.create({
     userPoolId: `${process.env.COGNITO_USERPOOLID}`,
@@ -53,6 +68,44 @@ const port = process.env.PORT || "3003";
 app.get("/", (Request, Response) => {
     Response.send("Welcome to the SQS API");
 });
+app.post("/new", (Request, Response) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a, _b, _c, _d, _e, _f;
+    if (((_a = Request.body) === null || _a === void 0 ? void 0 : _a.file) && ((_b = Request.body) === null || _b === void 0 ? void 0 : _b.userId) && ((_c = Request.body) === null || _c === void 0 ? void 0 : _c.jobId)) {
+        const params = {
+            QueueUrl: process.env.AWS_QUEUE_URL || "",
+            MessageBody: JSON.stringify({
+                file: (_d = Request.body) === null || _d === void 0 ? void 0 : _d.file,
+                userId: (_e = Request.body) === null || _e === void 0 ? void 0 : _e.userId,
+                jobId: (_f = Request.body) === null || _f === void 0 ? void 0 : _f.jobId,
+            }),
+        };
+        try {
+            const data = yield sqs.sendMessage(params).promise();
+            Response.send(`New SQS message sent. ID: ${data.MessageId}`);
+        }
+        catch (err) {
+            Response.status(400).send(err);
+        }
+    }
+    else {
+        Response.status(400).send("Body must include file, userId and jobId");
+    }
+}));
+app.get("/receive", (Request, Response) => __awaiter(void 0, void 0, void 0, function* () {
+    const params = {
+        QueueUrl: process.env.AWS_QUEUE_URL || "",
+        MaxNumberOfMessages: 1,
+        VisibilityTimeout: 20,
+        WaitTimeSeconds: 0,
+    };
+    try {
+        const data = yield sqs.receiveMessage(params).promise();
+        Response.send(data.Messages || []);
+    }
+    catch (err) {
+        Response.status(400).send(err);
+    }
+}));
 app.listen(port, () => {
     console.log(`SQS API running on port ${port}`);
 });
--- sqs_api/index.ts
+++ sqs_api/index.ts
@@ -1,11 +1,18 @@
 import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
 import * as dotenv from "dotenv";
 import { CognitoJwtVerifier } from "aws-jwt-verify";
+import AWS from "aws-sdk";
 
 const cors = require("cors");
 const app = express();
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 dotenv.config();
+
+// Configuration of AWS
+const sqs = new AWS.SQS({ apiVersion: process.env.API_VERSION });
 
 // Verifier that expects valid access tokens:
 const verifier = CognitoJwtVerifier.create({
@@ -16,8 +23,44 @@ const verifier = CognitoJwtVerifier.create({
 
 const port: string = process.env.PORT || "3003";
 
-app.get("/", (Request, Response) => {
+app.get("/", (Request: Request, Response: Response) => {
   Response.send("Welcome to the SQS API");
+});
+
+app.post("/new", async (Request: Request, Response: Response) => {
+  if (Request.body?.file && Request.body?.userId && Request.body?.jobId) {
+    const params = {
+      QueueUrl: process.env.AWS_QUEUE_URL || "",
+      MessageBody: JSON.stringify({
+        file: Request.body?.file,
+        userId: Request.body?.userId,
+        jobId: Request.body?.jobId,
+      }),
+    };
+    try {
+      const data = await sqs.sendMessage(params).promise();
+      Response.send(`New SQS message sent. ID: ${data.MessageId}`);
+    } catch (err) {
+      Response.status(400).send(err);
+    }
+  } else {
+    Response.status(400).send("Body must include file, userId and jobId");
+  }
+});
+
+app.get("/receive", async (Request: Request, Response: Response) => {
+  const params = {
+    QueueUrl: process.env.AWS_QUEUE_URL || "",
+    MaxNumberOfMessages: 1,
+    VisibilityTimeout: 20,
+    WaitTimeSeconds: 0,
+  };
+  try {
+    const data = await sqs.receiveMessage(params).promise();
+    Response.send(data.Messages || []);
+  } catch (err) {
+    Response.status(400).send(err);
+  }
 });
 
 app.listen(port, () => {
