8034e542cdfa61eedee6e4bc37814ff5e96b43c0
Peter Heywood <peterheywood708@hotmail.com>
2025-09-05 23:10:48 +0100

    Speech api now outputs object

--- speech_api/main.py
+++ speech_api/main.py
@@ -16,7 +16,7 @@ load_dotenv()
 workFolder = 'work'
 inFolder = 'in'
 
-# Our class for diarization and transcriptions
+# Our class for diarizations
 class Diarization:
     def __init__(self, speaker, text, start, end):
         self.speaker = speaker
@@ -39,10 +39,8 @@ pipeline = Pipeline.from_pretrained(
     "pyannote/speaker-diarization-3.1",
     token=os.getenv('TOKEN'))
 
-file = 'in\\1755643752826_2013-5-documentary-1-historical-alex-warner.mp3'
-
 # Function to start diarization once file is downloaded from s3
-def startDiarization(file):
+def startDiarization(file, userId, jobId):
     diarizationTranscriptions = []
     diarization = pipeline(file)
     for turn, _,speaker in diarization.itertracks(yield_label=True):
@@ -67,57 +65,62 @@ def startDiarization(file):
         finally:
             clipToTranscribe.close()
             os.remove(clipFileName)
-    print(json.dumps([Diarization.__dict__ for Diarization in diarizationTranscriptions]))
+    return json.dumps({"userId":userId,"jobId":jobId,"diarizations":[Diarization.__dict__ for Diarization in diarizationTranscriptions]})
 
-startDiarization(file)
+# Function to check for new messages from AWS SQS
+def checkMessages():
+    print("Checking for new messages..")
+    session = requests.Session()
+    res = session.get(os.getenv('SQS_API')+'/receive', headers={'Content-Type': 'application/json'})
+    session.close()
+    if not res.text:
+        print("No new messages received")
+        return
+    try:
+        jsonRes = json.loads(res.text)
+        jsonBody = json.loads(jsonRes[0]['Body'])
+        if not jsonBody['key']:
+            print(f"[{datetime.datetime.now()}] No S3 key found")
+            return
+        key = jsonBody['key']
+        userId = jsonBody['userId']
+        jobId = jsonBody['jobId']
+        session = requests.Session()
+        s3Res = session.get(os.getenv('S3_API')+'/retrieve', headers={'Content-Type': 'application/json','Key': key})
+        if not s3Res.text:
+            print(f"[{datetime.datetime.now()}] Unable to retrieve file URL from S3 using key {key}")
+            return
+        print(f"[{datetime.datetime.now()}] Downloading {key} from {s3Res.text}")
+        inFile = downloadFile(s3Res.text, key)
+        print(f"[{datetime.datetime.now()}] {key} downloaded to {inFile}")
+        if not inFile:
+            return
+        # Call our diarizations functions to start splicing and transcribing speakers
+        diarizations = startDiarization(inFile, userId, jobId)
+        # Finally save the transcriptions to the db
+        print(diarizations)
+        #Delete the file from S3 bucket
+        inFile.close()
+        os.remove(inFile)
+    except Exception as error:
+        print(f"[{datetime.datetime.now()}] {error}")
 
-# def checkMessages():
-#     print("Checking for new messages..")
-#     session = requests.Session()
-#     res = session.get(os.getenv('SQS_API')+'/receive', headers={'Content-Type': 'application/json'})
-#     session.close()
-#     if not res.text:
-#         print("No new messages received")
-#         return
-#     try:
-#         jsonRes = json.loads(res.text)
-#         jsonBody = json.loads(jsonRes[0]['Body'])
-#         if not jsonBody['key']:
-#             print(f"[{datetime.datetime.now()}] No S3 key found")
-#             return
-#         key = jsonBody['key']
-#         session = requests.Session()
-#         s3Res = session.get(os.getenv('S3_API')+'/retrieve', headers={'Content-Type': 'application/json','Key': key})
-#         if not s3Res.text:
-#             print(f"[{datetime.datetime.now()}] Unable to retrieve file URL from S3 using key {key}")
-#             return
-#         print(f"[{datetime.datetime.now()}] Downloading {key} from {s3Res.text}")
-#         inFile = downloadFile(s3Res.text, key)
-#         print(f"[{datetime.datetime.now()}] {key} downloaded to {inFile}")
-#         if not inFile:
-#             return
-#         startDiarization(inFile)
-#     except Exception as error:
-#         print(f"[{datetime.datetime.now()}] {error}")
+# Function to download file from S3 API service
+def downloadFile(url, key):
+    response = requests.get(url)
+    if not response.ok:
+        return
+    path = os.path.join(inFolder,key)
+    try:
+        with open(path, mode="wb") as file:
+            file.write(response.content)
+        return path
+    except Exception as err:
+        print(f"[{datetime.datetime.now()}] {err}")
+        return
 
-# # Function to download file from S3 API service
-# def downloadFile(url, key):
-#     response = requests.get(url)
-#     if not response.ok:
-#         return
-#     path = os.path.join(inFolder,key)
-#     try:
-#         with open(path, mode="wb") as file:
-#             file.write(response.content)
-#         return path
-#     except Exception as err:
-#         print(f"[{datetime.datetime.now()}] {err}")
-#         return
+schedule.every(10).seconds.do(checkMessages)
 
-
-
-# schedule.every(10).seconds.do(checkMessages)
-
-# while True:
-#     schedule.run_pending()
-#     time.sleep(1)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
