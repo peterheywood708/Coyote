1f9860b51890d4d4a57718202df14c02db716963
Peter Heywood <peterheywood708@hotmail.com>
2025-09-18 21:58:49 +0100

    Added dockerfile for frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,9 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   define: { "process.env": {}, global: {} },
+  server: {
+    host: true,
+    strictPort: true,
+    port: 8080,
+  },
 });
