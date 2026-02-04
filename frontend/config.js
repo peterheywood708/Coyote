707e6061cd9842b55813feebeb0cf40c75b37f73
Peter Heywood <peterheywood708@hotmail.com>
2026-02-04 20:20:16 +0000

    Added config.js

---
+++ frontend/config.js
@@ -0,0 +1,7 @@
+window.VITE_S3_ENDPOINT = undefined;
+window.VITE_DB_ENDPOINT = undefined;
+window.VITE_SQS_ENDPOINT = undefined;
+window.VITE_AWS_CLIENTID = undefined;
+window.VITE_AWS_AUTHORITY = undefined;
+window.VITE_AWS_REDIRECTURI = undefined;
+window.VITE_AWS_COGNITODOMAIN = undefined;
